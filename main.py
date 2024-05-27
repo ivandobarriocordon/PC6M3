970c081adb3d777b952b23bbe1b80fef7c5c8e72
Ivan Dobarrio <146997085+ivandobarriocordon@users.noreply.github.com>
2024-05-27 19:33:16 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+class Usuario:
+  def __init__(self, username, password):
+      self.username = username
+      self.password = password
+
+# Crear un objeto Usuario
+usuario = Usuario("ejemplo_usuario", "contraseña_secreta")
