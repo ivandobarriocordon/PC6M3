1791b7d20430f154c66265406b96ab5eb4af9908
Ivan Dobarrio <146997085+ivandobarriocordon@users.noreply.github.com>
2024-05-28 18:44:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ class Usuario:
 
 # Crear un objeto Usuario
 usuario = Usuario("ejemplo_usuario", "contraseña_secreta")
+
+# Imprimir el nombre de usuario y la contraseña del objeto Usuario
+print("Nombre de usuario:", usuario.username)
+print("Contraseña:", usuario.password)
